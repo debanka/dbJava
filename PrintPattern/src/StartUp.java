3a38ddcc984ce82d75805209ce8597e8074598ce
Debanka <dbnkbs@gmail.com>
2021-08-04 12:49:25 +0530

    Print The Pattern : https://practice.geeksforgeeks.org/problems/print-the-pattern-set-1/1

---
+++ PrintPattern/src/StartUp.java
@@ -0,0 +1,33 @@
+public class StartUp {
+
+    private static StringBuffer sb = new StringBuffer();;
+    private void printPat(int N)
+    {
+        int noOfPatterns = N,value = N;
+        while(noOfPatterns>0)
+        {
+            value = N;
+            while(value>0)
+            {
+                append(noOfPatterns,value);
+                value--;
+            }
+            sb.append('$');
+            noOfPatterns--;
+        }
+    }
+    private void append(int N,int value)
+    {
+        for(int index=0;index<N;index++)
+        {
+            sb.append(String.valueOf(value));
+        }
+    }
+    public static void main(String args[])
+    {
+        StartUp su = new StartUp();
+        su.printPat(3);
+        System.out.println(sb);
+
+    }
+}
